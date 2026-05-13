525389b0132ad0da115773064622522030bd445c
zekaeko.p <zekaeko.p@gmail.com>
2026-05-13 13:43:27 +0300

    init tutorial

---
+++ quickstart/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+from quickstart.serializers import UserSerializers, GroupSerializers
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializers
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializers
+    permission_classes = [permissions.IsAuthenticated]
