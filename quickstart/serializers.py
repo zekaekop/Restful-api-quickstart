596e2aecb7fe6adf02055efb88fe89c0dc1e1615
zekaeko.p <zekaeko.p@gmail.com>
2026-05-14 15:52:29 +0300

    users are now associated with snippets they create and have access to only the snippets they made, meaning they can delete their own snippets but not someone elses

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
-class UserSerializers(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
--- quickstart/views.py
+++ quickstart/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
-from quickstart.serializers import UserSerializers, GroupSerializers
+from quickstart.serializers import UserSerializer, GroupSerializers
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by("-date_joined")
-    serializer_class = UserSerializers
+    serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 class GroupViewSet(viewsets.ModelViewSet):
